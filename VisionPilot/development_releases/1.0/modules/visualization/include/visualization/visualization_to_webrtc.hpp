0a809d8e85bec5805c7d1d444bb903badde74c02
TranHuuNhatHuy <huy9515@gmail.com>
2026-05-18 14:28:25 +0900

    build first skeleton for header of vis image => webrtc

--- VisionPilot/development_releases/1.0/modules/visualization/include/visualization/visualization_to_webrtc.hpp
+++ VisionPilot/development_releases/1.0/modules/visualization/include/visualization/visualization_to_webrtc.hpp
@@ -1,8 +1,34 @@
 //
 // Created by atanasko on 1.5.26.
+// Developed by TranHuuNhatHuy on 18.5.26.
 //
 
 #ifndef VISIONPILOT_VISUALIZATION_TO_WEBRTC_H
 #define VISIONPILOT_VISUALIZATION_TO_WEBRTC_H
+
+#include <opencv2/opencv.hpp>
+#include <cstdint>
+#include <memory>
+#include <string>
+
+
+namespace visualization {
+
+    class VisualizationToWebRTC {
+    public:
+        VisualizationToWebRTC();
+        ~VisualizationToWebRTC();
+
+        void sendFrame(const cv::Mat& frame);
+        void setWebRTCConfig(const std::string& config);
+
+    private:
+        // Internal implementation details (e.g., WebRTC connection, encoding, etc.)
+        struct Impl;
+        std::unique_ptr<Impl> impl_;
+    };
+
+}
+
 
 #endif //VISIONPILOT_VISUALIZATION_TO_WEBRTC_H
